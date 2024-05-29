13c4102d8833a34d4abadb99a5d42a2b49d22dc2
Matthew <monkeytonk115@gmail.com>
2024-05-29 20:20:12 +0100

    fix bullet attachment

--- scenes/action/gun_action.gd
+++ scenes/action/gun_action.gd
@@ -1,4 +1,4 @@
 extends Node3D
 
 func fire():
-	get_node("/root/Main").shoot_bullet_client.rpc($lowResGun/gun_body/gun_barrel.global_transform, -$lowResGun/gun_body/gun_barrel.global_transform.basis.z * 200)
+	get_node("/root/Main").shoot_bullet_client.rpc($"Bullet spawn".global_transform, -$"Bullet spawn".global_transform.basis.z * 100)
