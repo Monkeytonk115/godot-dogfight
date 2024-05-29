2a3541a26ceedd1c6d0e9aa95d0f9c52ebc9bd30
Matthew <monkeytonk115@gmail.com>
2024-05-29 14:33:33 +0100

    multiplayer

--- scenes/interface/multiplayer_menu.gd
+++ scenes/interface/multiplayer_menu.gd
@@ -7,3 +7,11 @@ func _ready():
 
 func _process(delta):
 	pass
+
+
+func _on_host_multiplayer_pressed():
+	MultiplayerHelper.host_game()
+
+
+func _on_join_multiplayer_pressed():
+	MultiplayerHelper.join_game($Container/MarginContainer/VBoxContainer/AddrEntry.text)
---
+++ singletons/MultiplayerHelper.gd
@@ -0,0 +1,39 @@
+extends Node
+
+@onready var enet_peer = ENetMultiplayerPeer.new()
+
+func _ready():
+	# Emitted when this MultiplayerAPI's multiplayer_peer successfully connected to a server. Only emitted on clients.
+	multiplayer.connected_to_server.connect(connected_to_server)
+	
+	# Emitted when this MultiplayerAPI's multiplayer_peer fails to establish a connection to a server. Only emitted on clients.
+	multiplayer.connection_failed.connect(connection_failed)
+	
+	# Emitted when this MultiplayerAPI's multiplayer_peer connects with a new peer. ID is the peer ID of the new peer. Clients get notified when other clients connect to the same server. Upon connecting to a server, a client also receives this signal for the server (with ID being 1).
+	multiplayer.peer_connected.connect(peer_connected)
+	
+	# Emitted when this MultiplayerAPI's multiplayer_peer disconnects from a peer. Clients get notified when other clients disconnect from the same server.
+	multiplayer.peer_disconnected.connect(peer_disconnected)
+
+func host_game():
+	# if not hosting_game():
+	enet_peer.create_server(5555)
+	multiplayer.multiplayer_peer = enet_peer
+	print("hosting")
+
+func join_game(remote_addr):
+	enet_peer.create_client(remote_addr, 5555)
+	multiplayer.multiplayer_peer = enet_peer
+	print("joining")
+
+func connected_to_server():
+	print("connected to server")
+
+func connection_failed():
+	print("connection failed")
+
+func peer_connected(peer_id):
+	print("new peer connected: ", peer_id)
+
+func peer_disconnected(peer_id):
+	print("peer disconnected: ", peer_id)
