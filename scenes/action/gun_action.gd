065e4cb86d6c3fa794aff28a03e3b8a6706e81c4
Matthew <monkeytonk115@gmail.com>
2024-05-29 20:22:44 +0100

    fix typo

--- scenes/action/gun_action.gd
+++ scenes/action/gun_action.gd
@@ -1,4 +1,4 @@
 extends Node3D
 
 func fire():
-	get_node("/root/Main").shoot_bullet_client.rpc($bulletSpawn.global_transform, -$BulletSpawn.global_transform.basis.z * 100)
+	get_node("/root/Main").shoot_bullet_client.rpc($bulletSpawn.global_transform, -$bulletSpawn.global_transform.basis.z * 100)
