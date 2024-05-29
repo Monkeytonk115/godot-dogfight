275e57ccf2ba573f8119b289e432e1f2fafaedad
LilPiere <thomcat115@gmail.com>
2024-05-29 16:22:40 +0100

    plane flight

--- scenes/action/spitfire_action.gd
+++ scenes/action/spitfire_action.gd
@@ -11,13 +11,17 @@ func _physics_process(delta):
 	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+	var input_dir = Input.get_vector("Roll_Left", "Roll_Right", "Pitch_Up", "Pitch_Down")
+	var input_dir2 = Input.get_vector("Yaw_Left", "Yaw_Right", "ui_up", "ui_down")
+	
+	velocity = (transform.basis.z * -SPEED)
+	#Yaw
+	rotate(transform.basis.y, -input_dir2.x * delta)
+	#Pitch
+	rotate(transform.basis.x, input_dir.y * delta * 1.5)
+	#Roll
+	rotate(transform.basis.z, -input_dir.x * delta * 2)
+	
+	
 
 	move_and_slide()
