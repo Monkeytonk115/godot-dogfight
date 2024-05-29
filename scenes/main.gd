9dac5211d9a6f0f446ae7547b5985df09fe3a286
LilPiere <thomcat115@gmail.com>
2024-05-29 15:08:03 +0100

    colours

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,2 +1,11 @@
 extends Node3D
 
+const LANDSCAPE_V_1 = preload("res://scenes/world/landscapeV1.tscn")
+const SPITFIRE_ACTION = preload("res://scenes/action/spitfire_action.tscn")
+
+func _ready():
+	var new_landscape = LANDSCAPE_V_1.instantiate()
+	$world.add_child(new_landscape)
+	var new_spitfire = SPITFIRE_ACTION.instantiate()
+	$players.add_child(new_spitfire)
+	new_spitfire.position = Vector3(300, 300, 300)
