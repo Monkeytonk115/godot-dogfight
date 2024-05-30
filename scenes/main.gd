9f6f6a5c8ab691ef72760934dc18cb3b1c718942
Matthew <monkeytonk115@gmail.com>
2024-05-30 18:42:38 +0100

    added crashing and respawning, with spectate camera before respawn

--- scenes/main.gd
+++ scenes/main.gd
@@ -3,6 +3,8 @@ extends Node3D
 const LANDSCAPE_V_1 = preload("res://scenes/world/landscapeV1.tscn")
 const SPITFIRE_ACTION = preload("res://scenes/action/spitfire_action.tscn")
 
+var level = null
+
 func _ready():
 	MultiplayerHelper.change_level.connect(change_level)
 	MultiplayerHelper.player_change.connect(player_change)
@@ -11,9 +13,13 @@ func _ready():
 func change_level(new_level):
 	$CanvasLayer/MultiplayerMenu.hide()
 	print("changing level ", new_level)
-	var new_landscape = LANDSCAPE_V_1.instantiate()
-	$world.add_child(new_landscape)
+	if level:
+		level.queue_free()
+	level = LANDSCAPE_V_1.instantiate()
+	$world.add_child(level)
+	level.get_node("SpectateCamera").make_current()
 	spawn_planes()
+
 
 func spawn_planes():
 	var all_player_ids = multiplayer.get_peers()
@@ -25,15 +31,28 @@ func spawn_planes():
 		if ply_node:
 			print("player ", peer_id, " has plane already")
 			continue
-		var new_spitfire = SPITFIRE_ACTION.instantiate()
-		new_spitfire.set_multiplayer_authority(peer_id)
-		new_spitfire.set_name("spitfire" + str(peer_id))
-		$players.add_child(new_spitfire, true)
-		new_spitfire.position = Vector3(300, 300 + (hash(peer_id) % 40), 300)
+		spawn_plane(peer_id)
+
+
+func spawn_plane(peer_id):
+	var new_spitfire = SPITFIRE_ACTION.instantiate()
+	new_spitfire.set_multiplayer_authority(peer_id)
+	new_spitfire.set_name("spitfire" + str(peer_id))
+	$players.add_child(new_spitfire, true)
+	new_spitfire.position = Vector3(300, 300 + (hash(peer_id) % 40), 300)
+	new_spitfire.crashed.connect(plane_crashed)
 
 
 func player_change(peer_id, joined):
 	spawn_planes()
+
+
+func plane_crashed(plane : Node, peer_id : int):
+	print(plane.name, " crashed")
+	plane.queue_free()
+	if peer_id == multiplayer.get_unique_id():
+		level.get_node("SpectateCamera").make_current()
+	get_tree().create_timer(4).timeout.connect(func(): spawn_plane(peer_id))
 
 
 const bullet = preload("res://scenes/action/bullet.tscn")
